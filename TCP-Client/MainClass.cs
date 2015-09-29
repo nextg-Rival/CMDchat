772e1ffe086b3aca3beeb3fcd8f7e338ab8f37dd
nextg-Rival <nextg-Rival@users.noreply.github.com>
2015-09-29 02:05:52 +0100

    Update MainClass.cs

--- TCP-Client/MainClass.cs
+++ TCP-Client/MainClass.cs
@@ -14,7 +14,7 @@ namespace TCP_Client
             //login system V2
             string UNcompare;
             string Pcompare;
-            string pathToTxt = @"C:\Desktop\C#\Programs\Projects\TCP server\Login.txt";
+            string pathToTxt = @"...\TCP server\Login.txt";
             Console.WriteLine("Enter your username please!");
             string Username = Console.ReadLine();
             Console.WriteLine("Enter your password please!");
